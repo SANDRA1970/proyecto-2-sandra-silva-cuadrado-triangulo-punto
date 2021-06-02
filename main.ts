ff8df5737220c3562cdd56eb07606fa745f5b152
SANDRA1970 <84831652+SANDRA1970@users.noreply.github.com>
2021-06-01 19:00:44 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,139 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Square)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . # # #
+        . . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . # #
+        . . . . #
+        . . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . # . #
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . # . #
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . #
+        . # # # #
+        `)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . #
+        . . # # #
+        `)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.showString("Gracias!")
+})
